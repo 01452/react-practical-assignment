5017796dbb2785570a9976bec365203b9ed425c4
01452 <andreishahknovich@gmail.com>
2023-02-24 02:01:00 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
-import {useEffect} from 'react';
 import './App.css';
+import MainPage from './components/MainPage';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import { store } from './utils/store';
+import {Provider} from 'react-redux'
+
 
 function App() {
-
-  useEffect(() => {
-    // TEST API, it might be removed
-    fetch('http://localhost:8080/live').then(res => res.json()).then(res => {
-      console.log('API CONNECTION IS OK');
-    }).catch((e) => console.error('API CONNECTION FAILED, PLEASE CHECK SERVER APP AND TRY AGAIN'))
-  }, []);
-
   return (
-    <div className="App">
-      TASK IMPLEMENTATION HERE
-    </div>
+    <Provider store={store}>
+        <MainPage />
+    </Provider>
   );
 }
 
